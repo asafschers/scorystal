23481822dd3a20bf564451d9c63cde4da65a0836
asaf schers <schers@riskified.com>
2017-04-28 01:42:21 +0300

    start simple set predicate specs

--- spec/scorystal_spec.cr
+++ spec/scorystal_spec.cr
@@ -1,9 +1,30 @@
 require "./spec_helper"
 
+
 describe Scorystal do
   # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  context "quotes" do
+
+    it "returns true" do
+      pred_string = "<SimpleSetPredicate field=\"f36\" booleanOperator=\"isIn\">
+                        <Array n=\"6\" type=\"string\">&quot;Missing&quot;   &quot;No Match&quot;</Array>
+                        </SimpleSetPredicate>"
+
+      categorical_predicate = SimpleSetPredicate.new(pred_string)
+
+      categorical_predicate.true?({f36: "No Match"}).should eq(true)
+    end
+
+    it "returns false" do
+      pred_string = "<SimpleSetPredicate field=\"f36\" booleanOperator=\"isIn\">
+                        <Array n=\"6\" type=\"string\">&quot;Missing&quot;   &quot;No Match&quot;</Array>
+                        </SimpleSetPredicate>"
+
+      categorical_predicate = SimpleSetPredicate.new(pred_string)
+
+      categorical_predicate.true?({f36: "Match"}).should eq(false)
+    end
   end
+
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/scorystal"
+require "../src/*"

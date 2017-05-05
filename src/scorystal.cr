57a8321183d8e88f63c5b05ed45871b85ac0b3f3
asaf schers <schers@riskified.com>
2017-05-05 11:41:43 +0300

    Add json to hash logic to main module

--- src/scorystal.cr
+++ src/scorystal.cr
@@ -1,7 +1,18 @@
 require "./scorystal/*"
+require "json"
 
 module Scorystal
 
   class_property  logger = Logger.new(STDOUT)
 
+  def self.features_hash(json)
+    features = JSON.parse(json)
+    hash_features = {}  of String => String
+    features.each { |k, v|
+      hash_features[k.to_s] = v.to_s
+      hash_features[k.to_s] = "f" if v.to_s == "false"
+      hash_features[k.to_s] = "t" if v.to_s == "true"
+    }
+    hash_features
+  end
 end
